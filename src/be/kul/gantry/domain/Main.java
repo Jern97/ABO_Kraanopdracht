4e116d487ef8794192a61ca65c163725c855cefd
Jeroen Gardeyn <jeroen.gardeyn@hotmail.com>
2018-10-19 12:47:40 +0200

    main toegevoegd

---
+++ src/be/kul/gantry/domain/Main.java
@@ -0,0 +1,15 @@
+package be.kul.gantry.domain;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String [ ] args){
+        try{
+            Problem problem = Problem.fromJson(new File("1_10_100_4_FALSE_65_50_50.json"));
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
