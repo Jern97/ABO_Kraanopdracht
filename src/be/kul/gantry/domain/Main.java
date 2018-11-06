e8a6b4b63bc1afc6da03d3c042b3d6594e4920bc
Jeroen Gardeyn <jeroen.gardeyn@hotmail.com>
2018-11-06 19:21:43 +0100

    Main aangepast zoals het moet ingediend worden

--- src/be/kul/gantry/domain/Main.java
+++ src/be/kul/gantry/domain/Main.java
@@ -7,18 +7,17 @@ import java.io.IOException;
 import java.util.List;
 
 public class Main {
+    static String INPUT_FILE = "1_10_100_4_FALSE_65_50_50.json";
+    static String OUTPUT_FILE = "output.csv";
+
     public static void main(String [ ] args){
         try{
-            Problem problem = Problem.fromJson(new File("1_10_100_4_FALSE_65_50_50.json"));
+            Problem problem = Problem.fromJson(new File(INPUT_FILE));
 
-            BufferedWriter writer = new BufferedWriter(new FileWriter("output.csv"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT_FILE));
             writer.write("\"gID\";\"T\";\"x\";\"y\";\"itemsInCraneID\"");
 
-            long startTime = System.currentTimeMillis();
-            List<Move> moves = problem.solve();
-            System.out.println(System.currentTimeMillis() - startTime);
-
-            for(Move m: moves){
+            for(Move m: problem.solve()){
                 writer.write("\n");
                 writer.write(m.toString());
             }
